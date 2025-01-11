f078b8cce9e6a4c09296d6538a2cf7df8669c745
Bhavin-2004 <bhavinsali1470@gmail.com>
2025-01-11 19:18:25 +0530

    Finished

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".card-btn");
+const card = document.querySelector(".card")
+
+button.addEventListener("click", () => {
+    card.classList.toggle("change")
+})
+
